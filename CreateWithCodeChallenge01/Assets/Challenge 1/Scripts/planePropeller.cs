3cd90b1f481385c0d33796ffa392857c9a61c123
Gaberson19 <gaberson19@outlook.com>
2021-04-13 16:45:11 +0800

    importing from Unity

---
+++ CreateWithCodeChallenge01/Assets/Challenge 1/Scripts/planePropeller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class planePropeller : MonoBehaviour
+{
+    public GameObject propeller;
+    public float rotateSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotateSpeed);
+    }
+}
